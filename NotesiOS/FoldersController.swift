c4f331d24fcf52c901e58966c7f44f13a5882534
Nabil Rahman <nabilr@outlook.com>
2019-10-23 20:01:53 -0600

    Generate cells in a UITableView

---
+++ NotesiOS/FoldersController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  NotesiOS
+//
+//  Created by Nabil Rahman on 10/23/19.
+//  Copyright © 2019 Nabil Rahman. All rights reserved.
+//
+
+import UIKit
+
+extension FoldersController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath)
+        cell.textLabel?.text = "The first note! yay!"
+        return cell
+    }
+}
+
+class FoldersController: UITableViewController {
+
+    fileprivate let CELL_ID: String = "CELL_ID"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+        navigationItem.title = "Folders"
+        
+        setupTableView()
+    }
+    
+    fileprivate func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: CELL_ID)
+    }
+
+
+}
+
