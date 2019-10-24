c20a0aeaad81b0775117730bb68212e206f0bf54
Nabil Rahman <nabilr@outlook.com>
2019-10-23 20:26:50 -0600

    Implement the page for folder notes

---
+++ NotesiOS/Controllers/FolderNotesController.swift
@@ -0,0 +1,16 @@
+//
+//  FolderNotesController.swift
+//  NotesiOS
+//
+//  Created by Nabil Rahman on 10/23/19.
+//  Copyright © 2019 Nabil Rahman. All rights reserved.
+//
+
+import UIKit
+
+class FolderNotesController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Folder Notes"
+    }
+}
--- NotesiOS/Controllers/FoldersController.swift
+++ NotesiOS/Controllers/FoldersController.swift
@@ -13,11 +13,18 @@ extension FoldersController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 4
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath)
         cell.textLabel?.text = "The first note! yay!"
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let folderNotesController = FolderNotesController();
+        navigationController?.pushViewController(folderNotesController, animated: true)
+    }
+    
 }
 
 class FoldersController: UITableViewController {
@@ -27,7 +34,7 @@ class FoldersController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
         navigationItem.title = "Folders"
         
         setupTableView()
